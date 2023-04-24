ffd1880e5918f00ae2f3f65236dc48629279706e
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-24 11:48:30 -0600

    Feat: first part of extension, doctor names

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe 'Hospital Show Page', type: :feature do
     @hospital_2 = Hospital.create!(name: 'Judes Hospital')
 
     @doctor_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Medicine University')
-    @doctor_2 = @hospital_2.doctors.create!(name: 'John Doe', specialty: 'Pediatric Surgery', university: 'More Medicine University')
+    @doctor_2 = @hospital_1.doctors.create!(name: 'John Doe', specialty: 'Pediatric Surgery', university: 'More Medicine University')
+    @doctor_3 = @hospital_1.doctors.create!(name: 'Pac Man', specialty: 'Ghost Surgery', university: 'Fruit Snackin University')
+    
+    @doctor_4 = @hospital_2.doctors.create!(name: 'Jack Rack', specialty: 'General Surgery', university: 'University')
+    @doctor_5 = @hospital_2.doctors.create!(name: 'Jack Black', specialty: 'Not General Surgery', university: 'University of Learnin Doctors')
 
     @patient_1 = Patient.create!(name: 'Jim Carey', age: 24)
     @patient_2 = Patient.create!(name: 'Denny Denise', age: 39)
@@ -23,8 +27,19 @@ RSpec.describe 'Hospital Show Page', type: :feature do
 
       expect(page).to have_content(@hospital_1.name)
     end
-    
-    it 'I see the names of all the doctors that work at this hospital'
+
+    it 'I see the names of all the doctors that work at this hospital' do
+      visit hospital_path(@hospital_1)
+
+      within "#doctor-info" do
+        expect(page).to have_content(@doctor_1.name)
+        expect(page).to have_content(@doctor_2.name)
+        expect(page).to have_content(@doctor_3.name)
+
+        expect(page).to_not have_content(@doctor_4.name)
+        expect(page).to_not have_content(@doctor_5.name)
+      end
+    end
     it 'next to each doctor is the number of patients associate with them'
     it 'orders the from most number of patients to least'
   end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
+  before(:each) do
+    @hospital_1 = Hospital.create!(name: 'Memorial Hospital')
+    @hospital_2 = Hospital.create!(name: 'Judes Hospital')
+
+    @doctor_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Medicine University')
+    @doctor_2 = @hospital_1.doctors.create!(name: 'John Doe', specialty: 'Pediatric Surgery', university: 'More Medicine University')
+    @doctor_3 = @hospital_1.doctors.create!(name: 'Pac Man', specialty: 'Ghost Surgery', university: 'Fruit Snackin University')
+    
+    @doctor_4 = @hospital_2.doctors.create!(name: 'Jack Rack', specialty: 'General Surgery', university: 'University')
+    @doctor_4 = @hospital_2.doctors.create!(name: 'Jack Black', specialty: 'Not General Surgery', university: 'University of Learnin Doctors')
+
+    @patient_1 = Patient.create!(name: 'Jim Carey', age: 24)
+    @patient_2 = Patient.create!(name: 'Denny Denise', age: 39)
+    @patient_3 = Patient.create!(name: 'Sally Sue', age: 19)
+
+    PatientDoctor.create!(patient_id: @patient_1.id, doctor_id: @doctor_1.id)
+    PatientDoctor.create!(patient_id: @patient_2.id, doctor_id: @doctor_1.id)
+    PatientDoctor.create!(patient_id: @patient_3.id, doctor_id: @doctor_2.id)
+  end
+
   it {should have_many :doctors}
+
+  describe 'instance methods' do
+    describe '#doctor_names' do
+      it 'lists the doctors names' do
+        expect(@hospital_1.doctor_names).to eq([@doctor_1, @doctor_2, @doctor_3])
+      end
+    end
+  end
 end
