09d8fdc15f96d75f4944d71410a49cb7018b4be5
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-24 09:55:07 -0600

    Feat: finish doctor show page for user story 1

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Doctor Show Page', type: :feature do
   before(:each) do
     @hospital_1 = Hospital.create!(name: 'Memorial Hospital')
-    @hospital_2 = Hospital.create!(name: 'Not Memorial Hospital')
+    @hospital_2 = Hospital.create!(name: 'Judes Hospital')
 
     @doctor_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Medicine University')
     @doctor_2 = @hospital_2.doctors.create!(name: 'John Doe', specialty: 'Pediatric Surgery', university: 'More Medicine University')
@@ -23,13 +23,52 @@ RSpec.describe 'Doctor Show Page', type: :feature do
 
       within '#doctor-info' do
         expect(page).to have_content(@doctor_1.name)
-        expect(page).to have_content(@doctor_1.specialty)
-        expect(page).to have_content(@doctor_1.university)
+        expect(page).to have_content("Specialty: #{@doctor_1.specialty}")
+        expect(page).to have_content("University: #{@doctor_1.university}")
+      end
+
+      visit doctor_path(@doctor_2)
+
+      within '#doctor-info' do
+        expect(page).to have_content(@doctor_2.name)
+        expect(page).to have_content("Specialty: #{@doctor_2.specialty}")
+        expect(page).to have_content("University: #{@doctor_2.university}")
       end
     end
 
-    it 'I can see the name of the hospital where this doctor works'
-    it 'I can see the names of all of the patients this doctor has'
+    it 'I can see the name of the hospital where this doctor works' do
+      visit doctor_path(@doctor_1)
+
+      within '#hospital-info' do
+        expect(page).to have_content("Hospital: #{@hospital_1.name}")
+        expect(page).to_not have_content(@hospital_2.name)
+      end
+
+      visit doctor_path(@doctor_2)
+
+      within '#hospital-info' do
+        expect(page).to have_content("Hospital: #{@hospital_2.name}")
+        expect(page).to_not have_content(@hospital_1.name)
+      end
+    end
+
+    it 'I can see the names of all of the patients this doctor has' do
+      visit doctor_path(@doctor_1)
+
+      within '#patient-info' do
+        expect(page).to have_content(@patient_1.name)
+        expect(page).to have_content(@patient_2.name)
+        expect(page).to_not have_content(@patient_3.name)
+      end
+save_and_open_page
+      visit doctor_path(@doctor_2)
+
+      within '#patient-info' do
+        expect(page).to have_content(@patient_3.name)
+        expect(page).to_not have_content(@patient_1.name)
+        expect(page).to_not have_content(@patient_2.name)
+      end
+    end
 
 # As a visitor
 # When I visit a doctor's show page
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Doctor do
+  before(:each) do
+    @hospital_1 = Hospital.create!(name: 'Memorial Hospital')
+    @hospital_2 = Hospital.create!(name: 'Not Memorial Hospital')
+
+    @doctor_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Medicine University')
+    @doctor_2 = @hospital_2.doctors.create!(name: 'John Doe', specialty: 'Pediatric Surgery', university: 'More Medicine University')
+
+    @patient_1 = Patient.create!(name: 'Jim Carey', age: 24)
+    @patient_2 = Patient.create!(name: 'Denny Denise', age: 39)
+    @patient_3 = Patient.create!(name: 'Sally Sue', age: 19)
+
+    PatientDoctor.create!(patient_id: @patient_1.id, doctor_id: @doctor_1.id)
+    PatientDoctor.create!(patient_id: @patient_2.id, doctor_id: @doctor_1.id)
+    PatientDoctor.create!(patient_id: @patient_3.id, doctor_id: @doctor_2.id)
+  end
+  
   it {should belong_to :hospital}
   it {should have_many :patient_doctors}
   it {should have_many(:patients).through(:patient_doctors)}
+
+  describe 'instance methods' do
+    describe '#hospital_name' do
+      it 'lists the doctors hospital name' do
+        expect(@doctor_1.hospital_name).to eq(@hospital_1.name)
+        expect(@doctor_2.hospital_name).to eq(@hospital_2.name)
+      end
+    end
+    
+    describe '#doctor_patients' do
+      it 'lists the doctors patients names' do
+        expect(@doctor_1.doctor_patients).to eq([@patient_1.name, @patient_2.name])
+        expect(@doctor_2.doctor_patients).to eq([@patient_3.name])
+      end
+    end
+  end
 end
